53540cbe08a2989b9ac83342c3fadf4bf0e907f5
René Olivo <rene.olivo.do@gmail.com>
2019-11-11 11:41:51 -0400

    Revert "CATL-1128: Fix open case details"

--- ang/civicase-base/services/case-type.service.js
+++ ang/civicase-base/services/case-type.service.js
@@ -7,28 +7,8 @@
    * CaseType Service
    */
   function CaseType () {
-    var caseTypes = CRM['civicase-base'].caseTypes;
-
-    this.getAll = getAll;
-    this.getTitlesForNames = getTitlesForNames;
-
-    /**
-     * @returns {object[]} a list of case types.
-     */
-    function getAll () {
-      return caseTypes;
-    }
-
-    /**
-     * Returns a list of case type titles for the given names.
-     *
-     * @param {string[]} caseTypeNames the case type names.
-     * @returns {string[]} a list of case type titles.
-     */
-    function getTitlesForNames (caseTypeNames) {
-      return _.map(caseTypeNames, function (caseTypeName) {
-        return _.findWhere(caseTypes, { name: caseTypeName }).title;
-      });
-    }
+    this.getAll = function () {
+      return CRM['civicase-base'].caseTypes;
+    };
   }
 })(angular, CRM.$, CRM._, CRM);
