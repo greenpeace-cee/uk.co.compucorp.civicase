43522d54857b044b27c97832170e9aff396347ce
Swastik Pareek <swastik1608@gmail.com>
2020-02-04 05:31:38 +0000

    CATL-1113: Fix code comment format and improve tests language

--- ang/test/civicase/case/details/directives/case-details.directive.spec.js
+++ ang/test/civicase/case/details/directives/case-details.directive.spec.js
@@ -390,7 +390,7 @@
           caseid: $scope.item.id,
           atype: '3',
           reset: 1,
-          cid: CasesUtils.getAllCaseClientContactIdsFromAllContacts($scope.item.contacts).join(',')
+          cid: CasesUtils.getAllCaseClientContactIds($scope.item.contacts).join(',')
         });
       });
 
